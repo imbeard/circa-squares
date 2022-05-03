d4b4f6555cc589981e9ebb2cee8544fa77dcd567
Andrea <andrea@imbeard.com>
2022-05-03 13:27:23 +0200

    test 26 seconds

--- functions/screenshot.js
+++ functions/screenshot.js
@@ -12,7 +12,6 @@ function isFullUrl(url) {
 }
 
 async function screenshot(url, { format, viewport, dpr = 1, withJs = true, wait, timeout = 8500 }) {
-  // Must be between 3000 and 8500
   timeout = Math.min(Math.max(timeout, 3000), 8500);
 
   const browser = await chromium.puppeteer.launch({
