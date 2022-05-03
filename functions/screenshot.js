d938bd005121c0a811950f56010f70dfa23f93c5
Andrea <andrea@imbeard.com>
2022-05-03 14:24:30 +0200

    test img

--- functions/screenshot.js
+++ functions/screenshot.js
@@ -257,14 +257,17 @@ async function handleInstagram(url, page, timeout) {
   await page.type('[name=username]', 'elbarbabrb');
   await page.type('[type="password"]', 'cn4Wi3DpKDc6Jv');
   await page.click('[type=submit]');
-  await page.waitForNavigation();
+  //await page.waitForNavigation();
 
-  await page.goto(url);
+  
 /*   response = await page.waitForNavigation({
     waitUntil: 'networkidle0',
   }); */
-  response = await page.waitForSelector('[placeholder=Search]', { state: 'visible' });
-
+  await page.waitForSelector('[placeholder=Search]', { state: 'visible' });
+  await page.goto(url);
+  response = await page.waitForSelector('img', {
+      state: 'visible',
+    });
 
   return response;
 }
