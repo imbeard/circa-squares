ef793bffb09cc0ff8e938d134bd4ab89104e3122
Andrea <andrea@imbeard.com>
2022-06-03 21:10:47 +0200

    fix

--- functions/screenshot.js
+++ functions/screenshot.js
@@ -76,7 +76,7 @@ async function screenshot(url, { format, viewport, dpr = 1, withJs = true, wait,
 
   if(response === false) { // timed out, resolved false
     // await page.evaluate(() => window.stop()); // stop loading page to take screenshot anyway of what is on the page so far
-    await browser.close(); // OR close the browser mega test
+    await browser.close(); // OR close the browser
     throw new Error(`Timed out`); // throw error and do not return an image so it have to be requested again
   }
 
